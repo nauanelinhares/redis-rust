24885f4d80a607c269af56dfb8fcf055935d62bd
nauanelinhares <nauane.linhares@gmail.com>
2025-01-05 10:59:12 -0300

    feat: :sparkles: implement basic Redis commands (GET, SET) with in-memory storage

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,46 @@
 use mini_redis::{Connection, Frame};
 use tokio::net::{TcpListener, TcpStream};
 
-
-
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
-        process(socket).await;
+        tokio::spawn(async move {
+            process(socket).await;
+        });
     }
 }
 
-
-async fn process(socket : TcpStream) {
+async fn process(socket: TcpStream) {
     // The `Connection` lets us read/write redis **frames** instead of
     // byte streams. The `Connection` type is defined by mini-redis
 
+    use mini_redis::Command::{self, Get, Set};
+    use std::collections::HashMap;
+
+    // A hashmap is used to store data
+    let mut db = HashMap::new();
+
     let mut connection = Connection::new(socket);
- 
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
-        
 
-        // Respond with an error
-        let response = Frame::Error("unimplemented".to_string());
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
+
         connection.write_frame(&response).await.unwrap();
-
     }
 }
